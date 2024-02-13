09e229694d7bf8727b02797ab74ea0d8aee48762
riyaahh <142788178+riyaahh@users.noreply.github.com>
2024-02-13 22:28:53 +0530

    second

--- kfc/kfc/urls.py
+++ kfc/kfc/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from kfc import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
